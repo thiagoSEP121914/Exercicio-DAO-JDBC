d2026672f3379efe85d572c433bb5c31934a35a7
Thiago <thiagodacostaalves35@gmail.com>
2024-12-21 05:13:31 -0300

    Menu principal CRUD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,35 +7,87 @@ import entities.Product;
 
 import java.sql.Connection;
 import java.util.List;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ProdutoDao produtoDao = DaoFactory.createProductDao();
 
-        System.out.println("SELECT BY ID: \n");
-        Product product  = produtoDao.selectById(1);
-        System.out.println(product);
+            while (true) {
+                System.out.println("==========================================================");
+                System.out.println("S I S T E M A   D E  C A D A S T R O  D E  P R O D U T O S");
+                System.out.println("[1] CADASTRAR NOVO PRODUTO ");
+                System.out.println("[2] BUSCAR PRODUTO PELO ID ");
+                System.out.println("[3] BUSCAR TODOS OS PRODUTOS ");
+                System.out.println("[4] ATUALIZAR PRODUTO ");
+                System.out.println("[5] REMOVER PRODUTO");
+                System.out.println("[6] PARA SAIR:");
+                int num = sc.nextInt();
+                sc.nextLine();
 
-        System.out.println("SELECT ALL");
-        List<Product> list = produtoDao.selectAll();
-        list.forEach(System.out::println);
-        System.out.println();
+                switch (num) {
+                    case 1:
+                            System.out.print("Digite o nome do produto: ");
+                            String nome = sc.nextLine();
+                            System.out.print("Descricao: ");
+                            String descri = sc.nextLine();
+                            sc.nextLine();
+                            System.out.print("Preco");
+                            double preco = sc.nextDouble();
+                            Product product = new Product(null, nome, descri, preco);
+                            produtoDao.addProduct(product);
+                            System.out.println();
+                            break;
 
-        System.out.println("INSERT OBJETO");
-        Product prod = new Product(null, "zerbindi", "tendas", 33.5);
-        produtoDao.addProduct(prod);
-        System.out.println();
+                    case 2:
+                            System.out.print("DIgite o Id do produto: ");
+                            int id = sc.nextInt();
+                            System.out.println(produtoDao.selectById(id));
+                            System.out.println();
+                            break;
 
-        System.out.println("UPDATE OBJETO");
-        prod = produtoDao.selectById(6);
-        prod.setName("5 X 5");
-        produtoDao.updateProduct(prod);
-        System.out.println();
+                    case 3:
+                            List<Product> list = produtoDao.selectAll();
+                            list.forEach(System.out::println);
+                            break;
+                    case 4 :
+                        System.out.print("Qual produto deseja atualizar: ");
+                        id = sc.nextInt();
+                        Product prod = produtoDao.selectById(id);
+                        System.out.println(prod);
+                        System.out.println();
+                        System.out.print("Digite o nome do produto: ");
+                        nome = sc.nextLine();
+                        System.out.print("Descricao: ");
+                        descri = sc.nextLine();
+                        sc.nextLine();
+                        System.out.print("Preco");
+                        preco = sc.nextDouble();
+                        prod = new Product(null, nome, descri, preco);
+                        produtoDao.updateProduct(prod);
+                        System.out.println();
 
-        System.out.println("DELETE OBJETO");
-        produtoDao.removerProduto(1);
+                    case 5:
+                        System.out.print("Qual produto EXCLUIR: ");
+                        id = sc.nextInt();
+                        produtoDao.removerProduto(id);
+                        break;
 
+                    case 6:
+                        System.out.println("VOLTE SEMPRE!");
+                        sc.close();
+                        return;
+                    default:
+                        System.out.println("Opcao invalida. Tente novamente");
+                        break;
+                }
+
+
+
+
+            }
     }
 }
